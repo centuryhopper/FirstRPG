1dd722eab14ac0bf6dd6ac4ac31903aef5d71721
Leo Zhang <leoucfstudent@knights.ucf.edu>
2020-08-16 12:13:17 -0400

    added json practice files

---
+++ Assets/Scripts/Playground/Reading&Serializing JSON/QuestionCollection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+public class QuestionCollection
+{
+    public Question[] questions;
+    public string collectionName;
+
+    public override string ToString()
+    {
+        string result = "QUESTIONS\n";
+        StringBuilder sb = new StringBuilder();
+        foreach (Question question in questions)
+        {
+            sb.Append(string.Format($"Question: {question.text}\nAnswer: {question.answer}\n"));
+        }
+
+        return result + sb.ToString();
+    }
+}
---
+++ Assets/Scripts/Playground/Reading&Serializing JSON/QuestionLoader.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using TMPro;
+using Newtonsoft.Json;
+
+
+public class QuestionLoader : MonoBehaviour
+{
+
+    // path to the json file
+    string questionPath =
+    "Assets/Scripts/Playground/Reading&Serializing JSON/questions.json";
+
+    // collection of questions
+    QuestionCollection questionCollection;
+    TextMeshProUGUI words;
+    [SerializeField] bool shouldFormat = false;
+
+    void Awake()
+    {
+        words = FindObjectOfType<TextMeshProUGUI>();
+    }
+
+    [ContextMenu("Load Questions")]
+    void LoadQuestions()
+    {
+        // reads the file with path "questionPath" and closes it automatically
+        // after its done 
+        using (StreamReader reader = new StreamReader(questionPath))
+        {
+            // extract and store the entire file in a string
+            string json = reader.ReadToEnd();
+
+            // deserialize that string data into our question collection object
+            questionCollection = JsonUtility.FromJson<QuestionCollection>(json);
+        }
+
+        print("Questions loaded: " + questionCollection.questions.Length);
+        words.text = questionCollection.ToString();
+    }
+
+    [ContextMenu("Write Sample Questions")]
+    void WriteSampleQuestions()
+    {
+        GenerateSampleQuestions();
+
+        // writes data to the file with path "question path"
+        using (StreamWriter writer = new StreamWriter(questionPath))
+        {
+            // gets the data back in the form of a string and writes it
+            // back to the json file
+            string json = JsonUtility.ToJson(questionCollection, shouldFormat);
+            // string json = JsonConvert.SerializeObject(questionCollection, Formatting.Indented);
+            writer.Write(json);
+        }
+    }
+
+    void GenerateSampleQuestions()
+    {
+        // create new questions and store them into question collection
+        Question[] newQuestions = new Question[2] {
+            new Question() { text = "what month is my birthday?", answer = "september" },
+            new Question() { text = "how are you?", answer = "fine, thanks :D" }
+        };
+
+        questionCollection = new QuestionCollection() {
+            questions = newQuestions, collectionName = "samples"
+        };
+    }
+}
